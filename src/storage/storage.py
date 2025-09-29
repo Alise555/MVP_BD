f85a9d638c268df5d87f6c22386b3fd467ef6876
Soberjan <operations.rannsoknir@gmail.com>
2025-09-29 12:19:01 +0300

    Поправил сигнатуру update_data_file, убрал метод delete_from_data_file

--- src/storage/storage.py
+++ src/storage/storage.py
@@ -1,80 +1,71 @@
-# Storage.create_folder('new_folder', '')
+# Storage.create_folder(self, 'new_folder', '')
 
 class Storage:
-    def create_folder(folder_path: str):
+    def create_folder(self, folder_path: str):
         """
         Создать папку по пути folder_path
         Args:
-            folder_path(str): путь к создаваемой папке
+            folder_path(self, str): путь к создаваемой папке
         """
         # if not dbname:
             
         pass
     
-    def delete_folder(folder_path: str):
+    def delete_folder(self, folder_path: str):
         """
         Удалить папку по пути folder_path и все ее содержимое
         Args:
-            folder_path(str): путь к удаляемой папке
+            folder_path(self, str): путь к удаляемой папке
         """
-        # os.rmtree(folder_name+parent_folder)
-        # os.path.join()
+        # os.rmtree(self, folder_name+parent_folder)
+        # os.path.join(self, )
         pass
     
-    def rename_folder(folder_path: str, new_name: str):
+    def rename_folder(self, folder_path: str, new_name: str):
         """
         Переименовать папку по пути folder_path на new_name
         Args:
-            folder_path(str): путь к папке
-            new_name(str): новое название папки
+            folder_path(self, str): путь к папке
+            new_name(self, str): новое название папки
         """
         pass
     
     # Сигнатура методов, которые работают с data_file изменится, когда я начну их реализовывать!!!!!
-    def create_data_file(data_file_path: str):
+    def create_data_file(self, data_file_path: str):
         """
         Создать data_file
         Args:
-            data_file_path(str): путь к data_file
+            data_file_path(self, str): путь к data_file
         """
         pass
     
-    def insert_in_data_file(data_file_path: str, content: str):
+    def insert_in_data_file(self, data_file_path: str, content: str):
         """
-        Поместить в data_file новое содержимое content
+        Поместить в data_file новое содержимое content (self, вставляем данные в конец data_file)
         Args:
-            data_file_path(str): путь к data_file
-            content(str): содержимое, которое вставляем в data_file
+            data_file_path(self, str): путь к data_file
+            content(self, str): содержимое, которое вставляем в data_file
         """
         pass
         
-    def update_data_file(data_file: str, row_structure: str): # пока что стр, потом посмотрим, как лучше передавать
+    def update_data_file(self, data_file_path: str, new_content: str): # пока что стр, потом посмотрим, как лучше передавать
         """
-        Обновить в datafile содержимое, находящееся на позиции 
+        Перезаписать содержимое data_file новым содержимым new_content
         Args:
-            datafile_name(str): название data_file
-            new_content(str): новое содержимое, которое попадет в data_file
+            datafile_name(self, str): путь к обновляемому data_file
+            new_content(self, str): новое содержимое, которое попадет перезапишет data_file
         """
         pass
         
-    def get_from_data_file(data_file_path: str):
+    def get_from_data_file(self, data_file_path: str):
         """
         Получить содержимое data_file
         Args:
-            data_file_path(str): путь к data_file
+            data_file_path(self, str): путь к data_file
         """
         pass
         
-    def delete_from_data_file(data_file: str, conditions: str):
-        """
-        Удалить из data_file содержимое, удовлетворяющее некоторым условиям (подумаем, как это сделать)
-        Args: 
-            data_file(str): название data_file
-            conditions(str): условия, по которым удаляем
-        """
-        pass
-        
-    def delete_data_file(data_file_path: str):
+    def delete_data_file(self, data_file_path: str):
         """
         Удалить data_file
         Args:
@@ -82,7 +73,7 @@ class Storage:
         """
         pass
         
-    def create_metadata(metadata: dict, metadata_file_path: str):
+    def create_metadata(self, metadata: dict, metadata_file_path: str):
         """
         Создать файл с метаданными
         Args:
@@ -91,7 +82,7 @@ class Storage:
         """
         pass
         
-    def update_metadata(metadata: dict, metadata_file_path: str):
+    def update_metadata(self, metadata: dict, metadata_file_path: str):
         """
         Обновить файл с метаданными
         Args:
@@ -99,7 +90,7 @@ class Storage:
         """
         pass
         
-    def get_metadata(metadata_file_path: str) -> dict:
+    def get_metadata(self, metadata_file_path: str) -> dict:
         """
         Получить содержимое файла с метаданными
         Args:
@@ -107,7 +98,7 @@ class Storage:
         """
         pass
         
-    def delete_metadata(metadata_file_path: str):
+    def delete_metadata(self, metadata_file_path: str):
         """
         Удалить файл с метаданными
         Args:
@@ -115,7 +106,7 @@ class Storage:
         """
         pass
         
-    def create_index_file(index_file: str, type: str):
+    def create_index_file(self, index_file: str, type: str):
         """
         Создать индекс файл
         Args:
@@ -123,7 +114,7 @@ class Storage:
         """
         pass
         
-    def delete_index_file(index_file: str):
+    def delete_index_file(self, index_file: str):
         """
         Получить содержимое файла с метаданными
         Args:
@@ -131,7 +122,7 @@ class Storage:
         """
         pass
         
-    def update_index_file(index_file: str):
+    def update_index_file(self, index_file: str):
         """
         Удалить файл с метаданными
         Args:
