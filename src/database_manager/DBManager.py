3b1cee70991b531776582bf404bc551963b077c4
Дмитрий Мельников <>
2025-10-01 19:10:45 +0300

    DBManager: fix namings + return values

--- src/database_manager/DBManager.py
+++ src/database_manager/DBManager.py
@@ -1,5 +1,9 @@
+import os
+
 from typing import List
 from enum_status import Status
+from storage.storage import Storage
+from config.config import path
 
 
 class DBManager:
@@ -8,12 +12,12 @@ class DBManager:
     Использует внешнее хранилище для операций с папками и метаданными.
     """
 
-    def __init__(self, storage: str) -> None:
+    def __init__(self) -> None:
         """Инициализирует экземпляр DBManager.
         Args:
             storage (str): Объект хранилища.
         """
-        self._storage = storage
+        self._storage = Storage()
         self._databases = set()
         self._current_database = ""
 
@@ -24,20 +28,22 @@ class DBManager:
             metadata (Dict): Метаданные для базы данных.
         """
         if database_name in self._databases:
-            return Status.ERROR
-        result_create_folder = self._storage.create_folder(database_name)
+            raise Exception("Db already created")
+        result_create_folder = self._storage.create_folder(
+            os.path.join(path, database_name)
+        )
         if result_create_folder == Status.OK:
             self._databases.add(database_name)
             if self._databases:
-                result_metadata = self._storage.update_metadata(self._databases, "/")
+                result_metadata = self._storage.update_metadata(self._databases, path)
             else:
-                result_metadata = self._storage.create_metadata(self._databases, "/")
+                result_metadata = self._storage.create_metadata(self._databases, path)
             if result_metadata == Status.OK:
                 return Status.OK
             else:
-                return Status.ERROR
+                raise Exception("Broken metadata")
         else:
-            return Status.ERROR
+            raise Exception("Broken folder")
 
     def delete_database(self, database_name: str):
         """Удаляет базу данных.
@@ -45,17 +51,19 @@ class DBManager:
             database_name (str): Имя удаляемой базы данных.
         """
         if database_name not in self._databases:
-            return Status.ERROR
-        result_delete_folder = self._storage.delete_folder(database_name)
+            raise Exception("Unknown db")
+        result_delete_folder = self._storage.delete_folder(
+            os.path.join(path, database_name)
+        )
         if result_delete_folder == Status.OK:
             self._databases.remove(database_name)
-            result_metadata = self._storage.update_metadata(self._databases, "/")
+            result_metadata = self._storage.update_metadata(self._databases, path)
             if result_metadata == Status.OK:
                 return Status.OK
             else:
-                return Status.ERROR
+                raise Exception("Broken metadata")
         else:
-            return Status.ERROR
+            raise Exception("Broken folder")
 
     def update_database(self, old_database_name: str, new_database_name: str):
         """Переименовывает базу данных.
@@ -64,20 +72,20 @@ class DBManager:
             new_database_name (str): Новое имя базы данных.
         """
         if old_database_name not in self._databases:
-            return Status.ERROR
+            raise Exception("Unknown db")
         result_rename_folder = self._storage.rename_folder(
             old_database_name, new_database_name
         )
         if result_rename_folder == Status.OK:
             self._databases.remove(old_database_name)
             self._databases.add(new_database_name)
-            result_metadata = self._storage.update_metadata(self._databases, "/")
+            result_metadata = self._storage.update_metadata(self._databases, path)
             if result_metadata == Status.OK:
                 return Status.OK
             else:
-                return Status.ERROR
+                raise Exception("Broken metadata")
         else:
-            return Status.ERROR
+            raise Exception("Broken folder")
 
     def use_database(self, database_name: str):
         """Устанавливает активную базу данных.
@@ -85,13 +93,13 @@ class DBManager:
             database_name (str): Имя базы данных, которую нужно использовать.
         """
         if database_name not in self._databases:
-            return Status.ERROR
-        result_metadata = self._storage.get_metadata("/")
+            raise Exception("Unknown db_name")
+        result_metadata = self._storage.get_metadata(path)
         self._current_database = database_name
 
-    def show_database(self) -> List[str]:
+    def show_databases(self) -> List[str]:
         """Возвращает список всех баз данных."""
-        result_metadata = self._storage.get_metadata("/")
+        result_metadata = self._storage.get_metadata(path)
         return list(self._databases)
 
     def current_database(self):
